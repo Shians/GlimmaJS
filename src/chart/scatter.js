b6950f15927c9d5913ed5d94fec83e43f9a27839
Shians <registertonysu@gmail.com>
2016-03-14 16:53:11 +1100

    Corrected syntax
    - Corrected syntax.

--- src/chart/scatter.js
+++ src/chart/scatter.js
@@ -336,7 +336,7 @@ glimma.chart.scatterChart = function() {
 		if (!arguments.length) return searchValue;
 		searchValue = _;
 		return chart;
-	}
+	};
 
 	chart.idMap = function(_) {
 		if (!arguments.length) return idMap;
@@ -524,7 +524,7 @@ glimma.chart.scatterChart = function() {
 
 		function pxToNum(px) {
 			return +px.replace("px", "");
-		};
+		}
 
 		tooltipLeft = data.xJitter + xScale(xValue(data));
 		tooltipLeft += margin.left + margin.right;
