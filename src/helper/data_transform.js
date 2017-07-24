a0c0aa54851de039a04abbc42e9762b2d4370d0a
Shians <registertonysu@gmail.com>
2017-07-24 11:48:16 +1000

    Added toRowMajor function to transform

--- src/helper/data_transform.js
+++ src/helper/data_transform.js
@@ -1,14 +1,18 @@
 glimma.transform = {};
 
-glimma.transform.toD3Form = function(obj) {
-	let col_names = Object.keys(obj);
-	let n_row = obj[col_names[0]].length;
+glimma.transform.toRowMajor = function(obj) {
+	var col_names = Object.keys(obj);
+	var n_row = obj[col_names[0]].length;
 
-	return (_.map(_.range(n_row), function(i) {
-		let output = {};
-		for (col of col_names) {
-			output[col] = obj[col][i];
+	var row_range = _.range(n_row);
+	var output = _.map(row_range, function(i) {
+			var output = {};
+			for (var col_index = 0; col_index < col_names.length; col_index++) {
+				column = col_names[col_index];
+				output[column] = obj[column][i];
+			}
+			return output;
 		}
-		return output;
-	}));
+	);
+	return output
 }
